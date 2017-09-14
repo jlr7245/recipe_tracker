04826f0651c2d67a0ae161a76708faf26aae959f
J Silverstein <jess.pebble@gmail.com>
2017-09-14 09:54:31 -0400

    add associations

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,3 +1,6 @@
 class Ingredient < ApplicationRecord
   enum :category [:spice, :oil, :baking, :vegetable, :dairy, :meat, :other]
+
+  has_many :recipe_ingredients
+  has_many :recipes, through: :recipe_ingredients
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,6 @@
 class Recipe < ApplicationRecord
   enum course: [:breakfast, :lunch, :dinner, :dessert]
+
+  has_many :recipe_ingredients
+  has_many :ingredients, through: :recipe_ingredients
 end
