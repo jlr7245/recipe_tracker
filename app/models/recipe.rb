57ebdabf93d047ee238ba46da918d1bb21d92a30
J Silverstein <jess.pebble@gmail.com>
2017-09-14 10:59:14 -0400

    add scope

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -3,4 +3,6 @@ class Recipe < ApplicationRecord
 
   has_many :recipe_ingredients, dependent: :destroy
   has_many :ingredients, through: :recipe_ingredients
+
+  scope :recently_added, -> { where(created_at: (1.day.ago..Time.now))}
 end
